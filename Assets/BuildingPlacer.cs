3dce8ab4759e1c7d35d475d384751da464dad491
Dima Tiberiu <dima.tiberiu1@gmail.com>
2020-08-13 22:46:09 +0300

    Added go to GameObject and GoToClick

---
+++ Assets/BuildingPlacer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingPlacer : MonoBehaviour
+{
+   [SerializeField]
+   private GameObject buildingPrefab;
+
+   [SerializeField]
+   private KeyCode newObjectHotkey = KeyCode.A;
+
+   private GameObject _currentPlacingObject;
+
+   private float mouseWheelRotation = 0f;
+
+    private void Update()
+    {
+      HandleHotkeyPressed();
+      if (_currentPlacingObject != null) {
+         MoveCurrentObjectToMouse();
+         RotateFromMouseWheel();
+         ReleaseIfClicked();
+      }
+    }
+
+   private void RotateFromMouseWheel()
+   {
+      mouseWheelRotation += Input.mouseScrollDelta.y;
+      _currentPlacingObject.transform.Rotate(Vector3.up, mouseWheelRotation * 10f);
+   }
+
+   private void ReleaseIfClicked()
+   {
+      if (Input.GetMouseButtonDown(0)) {
+         _currentPlacingObject = null;
+      }
+   }
+
+   private void MoveCurrentObjectToMouse()
+   {
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+      if (Physics.Raycast(ray, out RaycastHit hitInfo)) {
+         _currentPlacingObject.transform.position = hitInfo.point;
+         _currentPlacingObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+      }
+
+   }
+
+   private void HandleHotkeyPressed()
+   {
+      if (Input.GetKeyDown(newObjectHotkey)) {
+         if (_currentPlacingObject == null) {
+            _currentPlacingObject = Instantiate(buildingPrefab);
+         } else {
+            Destroy(_currentPlacingObject);
+         }
+      }
+   }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+   public int speed = 1;
+
+   private Vector3 movementDirection = new Vector3();
+   private Vector3 movementThisFrame = new Vector3();
+   private RaycastHit hitInfo = new RaycastHit();
+
+   private Tree tree;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      //GoToTree();
+      GetUserInput();
+      MovePlayerToClick();
+   }
+   private void GetUserInput()
+   {
+      if(Input.GetMouseButtonDown(0)) {
+         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+         if(Physics.Raycast(ray, out hitInfo)) {
+            movementDirection = hitInfo.point - transform.position;
+         }
+      }
+   }
+
+   private void MovePlayerToClick()
+   {
+      if ()
+      movementThisFrame += new Vector3(movementDirection.x, 0, movementDirection.z) * speed * Time.deltaTime;
+      transform.position = movementThisFrame;
+   }
+
+
+   private void Harvest(Tree tree)
+   {
+      
+   }
+
+   private void GoToTree()
+   {
+      tree = FindObjectOfType<Tree>();
+      Vector3 heading = tree.transform.position - transform.position;
+      float distance = heading.magnitude;
+      Vector3 direction = heading / distance;
+
+      if (distance > 1) {
+         Vector3 movementThisFrame = direction * speed * Time.deltaTime;
+         transform.localPosition += movementThisFrame;
+      } else {
+         Harvest(tree);
+      }
+   }
+}
