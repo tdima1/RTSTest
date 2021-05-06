4ec3c878fb845840c74cf17729a9f4bfcdce05f1
Dima Tiberiu <dima.tiberiu1@gmail.com>
2021-05-06 16:15:28 +0300

    added grid

---
+++ Assets/MoveToClick.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToClick : MonoBehaviour
+{
+   public Camera mainCamera;
+   public LayerMask groundLayer;
+   public GameObject player;
+
+   private Grid grid;
+   private RaycastHit hitInfo;
+
+   void Awake()
+   {
+      grid = GetComponent<Grid>();
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if(Input.GetMouseButtonUp(0)) {
+         var destination = GetDestinationPoint();
+         print(destination);
+
+
+      }
+   }
+
+   private Vector2Int GetPositionInGrid(Vector3 point)
+   {
+      int gridPosX = Mathf.RoundToInt(point.x / grid.cellSize);
+      int gridPosZ = Mathf.RoundToInt(point.z / grid.cellSize);
+      return new Vector2Int(gridPosX, gridPosZ);
+   }
+
+   private Vector3 GetDestinationPoint()
+   {
+      Vector3 screenPosition = Input.mousePosition;
+      var mouseWorldPosition = mainCamera.ScreenPointToRay(screenPosition);
+
+      Physics.Raycast(mouseWorldPosition, out hitInfo, 100, groundLayer);
+
+      var positionInGrid = GetPositionInGrid(hitInfo.point) * grid.cellSize;
+
+      hitInfo.point = new Vector3(Mathf.FloorToInt(
+         positionInGrid.x),
+         0.001f,
+         positionInGrid.y);
+
+      return hitInfo.point;
+   }
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+//[ExecuteInEditMode]
+//[SelectionBase]
+public class Grid : MonoBehaviour
+{
+   public Camera mainCamera;
+   public int cellSize = 1;
+   public int gridSize = 10;
+
+   public GameObject cellPrefab;
+
+   void Awake()
+   {
+      mainCamera = Camera.main;
+      for (int i = 0; i < gridSize; i++) {
+         for (int j = 0; j < gridSize; j++) {
+            cellPrefab.transform.position = new Vector3(i, 0.001f, j);
+            Instantiate(cellPrefab, GameObject.FindGameObjectWithTag("Grid").transform);
+         }
+      }
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      
+   }
+}
